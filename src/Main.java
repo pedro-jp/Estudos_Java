b20700170bdbdc17567d739c3134f6918919b2c9
pedro-jp <joaopedroc035@gmail.com>
2024-12-10 23:08:21 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        byte smallest = 0;
+        short small = 10000;
+        int large = 100000;
+        long largest = 100000000L;
+        float smallestFloat = 0.0f;
+        double smallestDouble = 0.00000000d;
+        String name = "Joao";
+        boolean boo = false;
+        char letter = 'a';
+        int[] vetores = {1, 2, 3};
+
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("João");
+        System.out.println(lista.get(0));
+
+    }
+}
